8b8de99f1ac46d0f8a4596f27a5b72c64ae6f462
WeaselKek <pilgrimdestroyer@gmail.com>
2017-06-04 02:39:08 +0200

    Make it Bundem

---
+++ RAmBO/Jezici/eng.php
@@ -0,0 +1,72 @@
+<?php
+
+//index.php
+
+$L_REGSE='Register';
+$L_LOGSE='Login';
+$L_IZG='Lost something?';
+$L_NDJ='Found something?';
+
+
+$L_NDI='Latest things that were lost';
+$L_NDN='Latest things that were found';
+$L_PSI='Show all lost things';
+$L_PSN='Show all found things';
+
+//login.php & register.php
+
+$L_LERR='Incorrect login info';
+$L_RERR='Registration failed';
+$L_IERR='Edit failed';
+$L_LOG='Login';
+$L_REGIST='Register';
+$L_CHACC='Edit profile';
+$L_UNAM='Username';
+$L_PASS='Password';
+$L_PASSVER='Confirm password';
+$L_DRZAVA='Country';
+
+//spiskovi 
+
+$L_IZGNAM='Name of lost thing';
+$L_NADNAM='Name of found thing';
+$L_LOK='Location:';
+$L_DOK='Document';
+$L_ZIV='Animal';
+$L_ELU='Electronic device';
+$L_OST='Other';
+
+$L_DATOD='Date from:';   //DATUM OD??????
+$L_MESTO='Place:';
+$L_DATUM='Date:';
+
+
+//svoje_objave.php
+
+$L_MYOBJ='My notices';
+$L_NAZ='Name';
+$L_TIP='Type';
+$L_NAGRADA='Prize';
+$L_NAGRADALONG='Prize for finder:';
+$L_DELOBJ='Delete notice';
+$L_CHAOBJ='Edit notice';
+
+//lista_clanova.php
+
+$L_ULIST='User list';
+$L_UDEL='Delete user';
+
+//dodajNadjeno.php i dodajIzgubljeno.php
+
+$L_NADIZM='Edit information about found thing';
+$L_NADREP='Report found thing';
+$L_IZGIZM='Edit information about lost thing';
+$L_IZGREP='Report lost thing';
+$L_NAGUNOS='Prize (optional):';
+
+
+        
+
+
+
+?>
---
+++ RAmBO/Jezici/srb.php
@@ -0,0 +1,72 @@
+<?php
+
+//index.php
+
+$L_REGSE='Registruj se';
+$L_LOGSE='Prijavi se';
+$L_IZG='Izgubili ste nešto?';
+$L_NDJ='Našli ste nešto?';
+
+
+$L_NDI='Najskorije dodate izgubljene stvari';
+$L_NDN='Najskorije dodate nađene stvari';
+$L_PSI='Prikaži sve izgubljene stvari';
+$L_PSN='Prikaži sve nađene stvari';
+
+//login.php & register.php
+
+$L_LERR='Pogrešni podaci za prijavu.';
+$L_RERR='Neuspešna registracija.';
+$L_IERR='Neuspešna izmena.';
+$L_LOG='Prijava';
+$L_REGIST='Registracija';
+$L_CHACC='Izmeni Profil';
+$L_UNAM='Korisničko ime';
+$L_PASS='Lozinka';
+$L_PASSVER='Potvrda nove lozinke';
+$L_DRZAVA='Država';
+
+//spiskovi 
+
+$L_IZGNAM='Naziv izgubljene stvari';
+$L_NADNAM='Naziv nađene stvari';
+$L_LOK='Lokacija:';
+$L_DOK='Dokument';
+$L_ZIV='Životinja';
+$L_ELU='Elektronski uređaj';
+$L_OST='Ostalo';
+
+$L_DATOD='Datum od:';   //DATUM OD??????
+$L_MESTO='Mesto:';
+$L_DATUM='Datum:';
+
+
+//svoje_objave.php
+
+$L_MYOBJ='Moje objave';
+$L_NAZ='Naziv';
+$L_TIP='Tip';
+$L_NAGRADA='Nagrada';
+$L_NAGRADALONG='Nagrada za pronalazača:';
+$L_DELOBJ='Obriši objavu';
+$L_CHAOBJ='Izmeni objavu';
+
+//lista_clanova.php
+
+$L_ULIST='Lista članova';
+$L_UDEL='Obriši člana';
+
+//dodajNadjeno.php i dodajIzgubljeno.php
+
+$L_NADIZM='Izmeni informacije o nađenoj stvari';
+$L_NADREP='Prijavi nađenu stvar';
+$L_IZGIZM='Izmeni informacije o izgubljenoj stvari';
+$L_IZGREP='Prijavi izgubljenu stvar';
+$L_NAGUNOS='Nagrada (nije obavezno):';
+
+
+        
+
+
+
+?>
